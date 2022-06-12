f93ffdb1b2d3514f016bb139ad92bb7d80006d08
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-06-12 21:24:37 +0500

    header section complated destop and mobil device

---
+++ app.js
@@ -0,0 +1,20 @@
+let menuImgJs = document.querySelector('.menu-img'),
+    menuItemsJs = document.querySelector(".menu-items")
+
+menuItemsJs.style.display = "none";
+
+function menu() {
+    if (menuItemsJs.style.display =="none") {
+        menuItemsJs.style.display = "block"
+    }
+    else{
+        menuItemsJs.style.display = "none"
+    }
+}
+
+
+    
+
+
+
+
